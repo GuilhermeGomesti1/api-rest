9b87d2896ea5862f07ee448eb6c9acd2c8c49d2c
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-01 16:55:53 -0300

    create new project

--- src/app.js
+++ src/app.js
@@ -5,3 +5,7 @@ const port = 3000
 app.get('/', (req, res)  => {
    res.send('Olá mundo!')
 })
+
+app.listen(port, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${port}`)
+})
