ba317b97ba836a9999bc1b6fc11e1d4eb944350a
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-01 17:47:11 -0300

    att

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-const express = require('express')
+import express from 'express'
+
 const app = express()
-const port = 3000
+
 
 app.get('/', (req, res)  => {
    res.send('Olá mundo!')
 })
 
-app.listen(port, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${port}`)
-})
+export default app
