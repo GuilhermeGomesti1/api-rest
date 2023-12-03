374fac7b24967f7b4fc266b10113e6f935468a24
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-03 02:21:54 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ function buscarSelecaoPorId(id){
  return selecoes.filter( selecao => selecao.id == id)
 }
 
+function buscarIndexSelecao(id){
+   return selecoes.findIndex ( selecao => selecao.id == id)
+}
 
 app.get('/', (req, res)  => {
    res.status(200).send('Olá mundo!')
@@ -32,5 +35,19 @@ app.get('/selecoes/:id', (req, res) =>{
 app.post('/selecoes', (req, res)=>{
    selecoes.push(req.body)
    res.status(201).send('Seleção cadastrada com sucesso')
+})
+
+app.delete('/selecoes/:id', (req, res) =>{
+   let index = buscarIndexSelecao(req.params.id)
+   selecoes.splice(index, 1)
+   res.send(`selecao com id ${req.params.id} excluida com sucesso`)
+})
+
+
+app.put('/selecoes/:id', (req, res) =>{
+   let index = buscarIndexSelecao(req.params.id)
+   selecoes[index].selecao = req.body.selecao
+   selecoes[index].grupo = req.body.grupo
+   res.json(selecoes)
 })
 export default app
