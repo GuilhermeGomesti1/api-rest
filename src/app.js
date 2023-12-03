62f503ac988c298e747bf5e4a9d669eef4cf3259
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-03 01:38:50 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,35 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
+const selecoes = [
+   {id: 1, selecao: 'Brasil' , grupo: 'G'  },
+   {id: 2, selecao: 'Suiça' , grupo: 'G'  },
+   {id: 3, selecao: 'Camarões' , grupo: 'G'  },
+   {id: 4, selecao: 'Sérvia' , grupo: 'G'  },
+  
+]
+
+function buscarSelecaoPorId(id){
+ return selecoes.filter( selecao => selecao.id == id)
+}
+
 
 app.get('/', (req, res)  => {
-   res.send('Olá mundo!')
+   res.status(200).send('Olá mundo!')
 })
 
 app.get('/selecoes', (req, res)=>{
-   res.send('Lista de seleções')
+   res.send(selecoes)
+})
+
+app.get('/selecoes/:id', (req, res) =>{
+   res.json(buscarSelecaoPorId(req.params.id))
+})
+
+app.post('/selecoes', (req, res)=>{
+   selecoes.push(req.body)
+   res.status(201).send('Seleção cadastrada com sucesso')
 })
 export default app
